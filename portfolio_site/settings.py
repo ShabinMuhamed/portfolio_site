82e02439604019df436683298861b800a17b65f3
Shabin <shabinmohd369@gmail.com>
2025-07-10 19:26:41 +0530

    Switched to env vars for email config

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -136,8 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shabinmohd369@gmail.com'
-EMAIL_HOST_PASSWORD = 'vsdx vhej gryd yzgo'
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-CONTACT_RECEIVER_EMAIL = 'shabinmohd369@gmail.com' 
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+CONTACT_RECEIVER_EMAIL = os.environ.get('CONTACT_RECEIVER_EMAIL')
